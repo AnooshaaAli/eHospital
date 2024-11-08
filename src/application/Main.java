80282417fd30afc807016a37ed228542cc618044
AnooshaaAli <anooshasixteen@gmail.com>
2024-11-08 16:43:42 +0500

    BoilerPlate

---
+++ src/application/Main.java
@@ -0,0 +1,47 @@
+package application;
+	
+import java.sql.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class Main extends Application {
+	
+	private Connection connect() {
+	    String url = "jdbc:sqlserver://10N5Q8AKAMRA\\SQLEXPRESS01;databaseName=eHospital;integratedSecurity=true;trustServerCertificate=true";
+	    Connection conn = null;
+	    try {
+	        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // Load SQL Server JDBC driver
+	        conn = DriverManager.getConnection(url);
+	        System.out.println("Connected to the database successfully!");
+	    } catch (ClassNotFoundException e) {
+	        System.out.println("SQL Server JDBC Driver not found.");
+	        e.printStackTrace();
+	    } catch (SQLException e) {
+	        System.out.println("Database connection failed.");
+	        e.printStackTrace();
+	    }
+	    return conn;
+	}
+
+    
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		Connection conn = this.connect();
+	}
+    
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
