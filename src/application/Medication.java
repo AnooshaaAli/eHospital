56fe36603133f4f75ec5fe9f3634da8edb3cadf8
NNoorFatima <noor.2205.nf@gmail.com>
2024-11-14 15:47:28 +0500

    added medication class fields

---
+++ src/application/Medication.java
@@ -0,0 +1,32 @@
+package application;
+
+public class Medication {
+	private int dosage; //this is number of days
+	private String medicineName; //name of the medicine
+	
+	
+	public int getDosage() {
+		return dosage;
+	}
+	public void setDosage(int dosage) {
+		this.dosage = dosage;
+	}
+	public String getMedicineName() {
+		return medicineName;
+	}
+	public void setMedicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+	public void enterMedicationDetails(String name)
+	{}
+	public void updateMedicationDetails()
+	{
+		
+	}
+	public Medication showExistingMedicationDetails()
+	{
+		return null;
+	}
+	
+	
+}
