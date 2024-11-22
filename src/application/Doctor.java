10b71d10dedd50c81cf1c5821b760a4c279f37a7
AnooshaaAli <anooshasixteen@gmail.com>
2024-11-22 19:42:06 +0500

    Doctor Class

--- src/application/Doctor.java
+++ src/application/Doctor.java
@@ -7,17 +7,20 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 
 public class Doctor extends Employee{
+	private static Doctor instance; 
 	private int doctorId;
     private boolean[] availableDays; 
 	private int num_appointments;
-	private DBHandler dbhandler;
+	private static DBHandler dbhandler;
 	private List<TimeSlot> timeslots;
-	
-    Doctor() {
-        availableDays = new boolean[6]; 
-        dbhandler = new DBHandler();
-    }
     
+	public void init(int id, boolean[] available, int app) {
+		this.doctorId=id;
+		this.availableDays= available;
+		this.num_appointments=app;
+    }
+	
+	
     // ------------------------------------ POPULATE WITH DOCTOR ID'S ----------------------------------------------- //
     
 	public ObservableList<Integer> getDoctorIdsList() {
@@ -36,4 +39,110 @@ public class Doctor extends Employee{
 		return dbhandler.fetchAvailableTimeSlots(docId, date);
 	}
 	
+	public static Doctor getInstance() 
+	{
+		if (instance == null) {
+            instance = new Doctor();
+        }
+        dbhandler = new DBHandler();
+        return instance;
+	}
+	
+	public boolean[] convertDaysToBooleanArray(String days) {
+	    // Create a boolean array for Monday to Friday
+	    boolean[] workingDays = new boolean[5];
+	    
+	    // Days of the week (Monday to Friday)
+	    String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+	    
+	    // Split the input days string into an array of individual days
+	    String[] doctorDays = days.split(",\\s*"); // Assuming days are comma-separated, e.g., "Monday, Wednesday"
+	    
+	    // Iterate over the weekDays array and mark true if the day is in the doctorDays array
+	    for (int i = 0; i < weekDays.length; i++) {
+	        for (String doctorDay : doctorDays) {
+	            if (weekDays[i].equalsIgnoreCase(doctorDay)) {
+	                workingDays[i] = true;
+	                break;
+	            }
+	        }
+	    }
+	    
+	    return workingDays;
+	}
+	public String convertBooleanArrayToDays(boolean[] workingDays) {
+	    // Days of the week (Monday to Friday)
+	    String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+	    
+	    // Use a StringBuilder to build the resulting string
+	    StringBuilder workingDaysString = new StringBuilder();
+	    
+	    // Iterate over the boolean array
+	    for (int i = 0; i < workingDays.length; i++) {
+	        if (workingDays[i]) {
+	            if (workingDaysString.length() > 0) {
+	                workingDaysString.append(", ");
+	            }
+	            workingDaysString.append(weekDays[i]);
+	        }
+	    }
+	    
+	    return workingDaysString.toString();
+	}
+
+    public Doctor() {
+    	dbhandler = new DBHandler();
+        availableDays = new boolean[6]; 
+    }
+    public boolean LoginDoctor(String username,String pass)
+    {
+    	boolean check = dbhandler .LoginDoctor(username,pass);
+		return check;   	
+    }
+//    public String loadDoctorName(String username)
+//	{
+//		String data=db.loadDoctorName(username);
+//		
+//		return data;
+//	}
+	public int loadDoctorId(String username)
+	{
+		int data = dbhandler.loadDoctorId(username);
+		
+		return data;
+	}
+    public String loadDoctorWorkingDays(String username)
+    {
+    	
+    	String days= dbhandler.loadWorkingDays(username);
+    	return days;
+    }
+	public DBHandler getDb() {
+		return dbhandler;
+	}
+	public void setDb(DBHandler db) {
+		this.dbhandler = db;
+	}
+	public int getDoctorId() {
+		return doctorId;
+	}
+	public void setDoctorId(int doctorId) {
+		this.doctorId = doctorId;
+	}
+	public boolean[] getAvailableDays() {
+		return availableDays;
+	}
+	public void setAvailableDays(boolean[] availableDays) {
+		this.availableDays = availableDays;
+	}
+	public int getNum_appointments() {
+		return num_appointments;
+	}
+	public void setNum_appointments(int num_appointments) {
+		this.num_appointments = num_appointments;
+	}
+	public static void setInstance(Doctor instance) {
+		Doctor.instance = instance;
+	}
+    
 }
